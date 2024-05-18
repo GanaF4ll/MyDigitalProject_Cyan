cd532ea6677012c7539788b66ad3cec65797f650
GanaF4ll <ganafall9498@gmail.com>
2024-05-18 15:27:11 +0200

    :lipstick: creating styling base

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import style from "./constants/styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>??</Text>
+    <View style={style.container}>
+      <Text style={style.text_green_primary}>??</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ constants/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text_blue_primary: {
+    color: "#193762",
+  },
+  text_orange_primary: {
+    color: "#DB8B34",
+  },
+  text_green_primary: {
+    color: "#A3EDBF",
+  },
+  bg_blue_primary: {
+    backgroundColor: "#193762",
+  },
+  bg_orange_primary: {
+    backgroundColor: "#DB8B34",
+  },
+  bg_green_primary: {
+    backgroundColor: "#A3EDBF",
+  },
+});
