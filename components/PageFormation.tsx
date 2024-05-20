1ede3b85054d75acab5448ae960aa55d21638dd7
GanaF4ll <ganafall9498@gmail.com>
2024-05-20 12:31:26 +0200

    :sparkles: scrollview element

--- components/PageFormation.tsx
+++ components/PageFormation.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { Formation } from "./Formation";
 import styles from "../constants/styles";
@@ -7,20 +7,22 @@ import { FORMATIONS } from "../constants";
 export default function PageFormation() {
   return (
     <View style={styles.container}>
-      {FORMATIONS.map((formation) => (
-        <Formation
-          key={formation.id}
-          id={formation.id}
-          author={formation.author}
-          title={formation.title}
-          description={formation.description}
-          video={formation.video}
-          category={formation.category}
-          difficulty={formation.difficulty}
-          qualityRating={formation.qualityRating}
-          coverImage={formation.coverImage}
-        />
-      ))}
+      <ScrollView>
+        {FORMATIONS.map((formation) => (
+          <Formation
+            key={formation.id}
+            id={formation.id}
+            author={formation.author}
+            title={formation.title}
+            description={formation.description}
+            video={formation.video}
+            category={formation.category}
+            difficulty={formation.difficulty}
+            qualityRating={formation.qualityRating}
+            coverImage={formation.coverImage}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
