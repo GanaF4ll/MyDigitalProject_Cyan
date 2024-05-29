8de47577b2b9217f4e7f79e16cb2359605051b37
GanaF4ll <ganafall9498@gmail.com>
2024-05-29 19:51:28 +0200

    :sparkles: bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -5,10 +5,15 @@ import AppNavigator from "./navigation";
 import { AuthProvider, useAuth } from "./app/context/AuthContext";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import Home from "./app/screens/Home";
 import Login from "./app/screens/Login";
+import PageFormation from "./app/screens/PageFormation";
+import Search from "./app/screens/Search";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -17,27 +22,49 @@ export default function App() {
     </AuthProvider>
   );
 }
-
 export const Layout = () => {
   const { authState, onLogout } = useAuth();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {authState?.authenticated ? (
-          <Stack.Screen
+      {authState?.authenticated ? (
+        <Tab.Navigator>
+          <Tab.Screen
             name="Home"
             component={Home}
             options={{
+              tabBarLabel: "Home",
               headerRight: () => (
                 <Button onPress={onLogout} title="Déconnexion" />
               ),
             }}
-          ></Stack.Screen>
-        ) : (
-          <Stack.Screen name="Login" component={Login}></Stack.Screen>
-        )}
-      </Stack.Navigator>
+          />
+          <Tab.Screen
+            name="PageFormation"
+            component={PageFormation}
+            options={{
+              tabBarLabel: "Formation",
+              headerRight: () => (
+                <Button onPress={onLogout} title="Déconnexion" />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Search"
+            component={Search}
+            options={{
+              tabBarLabel: "Search",
+              headerRight: () => (
+                <Button onPress={onLogout} title="Déconnexion" />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,22 +1,20 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
-import * as SecureStore from "expo-secure-store";
 
 interface AuthProps {
-  authState?: { token: string | null; authenticated: boolean | null };
+  authState?: { authenticated: boolean | null };
   onRegister?: (
     firstName: string,
     lastName: string,
     birthdate: Date,
-    email: string,
+    mail: string,
     password: string
   ) => Promise<any>;
-  onLogin?: (email: string, password: string) => Promise<any>;
+  onLogin?: (mail: string, password: string) => Promise<any>;
   onLogout?: () => Promise<any>;
 }
 
-const TOKEN_SECRET = "";
-export const API_URL = "";
+const API_URL = "https://virtual-sentinel-5db263dece23.herokuapp.com";
 const AuthContext = createContext<AuthProps>({});
 
 export const useAuth = () => {
@@ -25,33 +23,18 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }: any) => {
   const [authState, setAuthState] = useState<{
-    token: string | null;
     authenticated: boolean | null;
   }>({
-    token: null,
     authenticated: null,
   });
 
-  useEffect(() => {
-    const loadToken = async () => {
-      // récupère le token du localStorage
-      const token = await SecureStore.getItemAsync(TOKEN_SECRET);
-      if (token) {
-        setAuthState({ token, authenticated: true });
-        // attache le token à toutes les requêtes axios
-        axios.defaults.headers.common["Authorization"] = `${token}`;
-      } else {
-        setAuthState({ token: null, authenticated: false });
-      }
-    };
-    loadToken();
-  }, []);
+  useEffect(() => {}, []);
 
   const register = async (
     firstName: string,
     lastName: string,
     birthdate: Date,
-    email: string,
+    mail: string,
     password: string
   ) => {
     try {
@@ -59,7 +42,7 @@ export const AuthProvider = ({ children }: any) => {
         firstName,
         lastName,
         birthdate,
-        email,
+        mail,
         password,
       });
     } catch (error) {
@@ -67,33 +50,25 @@ export const AuthProvider = ({ children }: any) => {
     }
   };
 
-  const login = async (email: string, password: string) => {
+  const login = async (mail: string, password: string) => {
     try {
-      const result = await axios.post(`${API_URL}/admin/login`, {
-        email,
+      const result = await axios.post(`${API_URL}/users/login`, {
+        mail,
         password,
       });
       console.log(result.data);
 
-      setAuthState({ token: result.data.token, authenticated: true });
-
-      // attache le token à toutes les requêtes axios
-      axios.defaults.headers.common["Authorization"] = `${result.data.token}`;
-
-      //   équivalent du localStorage pour React Native
-      await SecureStore.setItemAsync(TOKEN_SECRET, result.data.token);
+      setAuthState({ authenticated: true });
     } catch (error) {
+      console.log(error);
       return { error: true, msg: (error as any).response.data.msg };
     }
   };
 
   const logout = async () => {
-    //  supprime le token du localStorage
-    await SecureStore.deleteItemAsync(TOKEN_SECRET);
-    // supprime le token des requêtes axios
-    axios.defaults.headers.common["Authorization"] = null;
     // reset le state
-    setAuthState({ token: null, authenticated: false });
+
+    setAuthState({ authenticated: false });
   };
 
   const value = {
