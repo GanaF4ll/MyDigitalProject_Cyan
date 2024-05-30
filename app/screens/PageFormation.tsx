a674552c9bc3420a655c55a99034a6ef96d6374a
GanaF4ll <ganafall9498@gmail.com>
2024-05-30 16:56:06 +0200

    :sparkles::construction: fetching formations form the API

--- app/screens/PageFormation.tsx
+++ app/screens/PageFormation.tsx
@@ -1,10 +1,24 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { Formation } from "../components/Formation";
 import styles from "../constants/styles";
 import { FORMATIONS } from "../constants";
+import { API_URL } from "../constants/index";
+import { FormationType } from "../constants/types";
+import axios from "axios";
 
 export default function PageFormation() {
+  const [formations, setFormations] = useState<FormationType[]>([]);
+
+  useEffect(() => {
+    const fetchFormations = async () => {
+      const result = await axios.get(`${API_URL}/formations/all`);
+      setFormations(result.data);
+      console.log(result.data);
+    };
+    fetchFormations();
+  }, []);
+
   return (
     <View style={styles.container}>
       <ScrollView>
