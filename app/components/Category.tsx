d76a9fdc5526b24c43483a8693791392561528a9
GanaF4ll <ganafall9498@gmail.com>
2024-06-10 19:53:48 +0200

    :lipstick: Category component UI completed

--- app/components/Category.tsx
+++ app/components/Category.tsx
@@ -19,15 +19,49 @@ export default function Category() {
       <LinearGradient
         colors={["#370475", "red"]}
         start={{ x: 0, y: 0 }}
-        end={{ x: 2, y: 3 }}
+        end={{ x: 2, y: 1 }}
         style={categoryStyle.subContainer}
       >
-        <Text style={styles.title_white}>Fondamentaux de la cybersécurité</Text>
-        <Gradient onPress={() => console.log("yo")}>
-          <Text style={styles.title_white}>
-            Continuer <FontAwesome name="arrow-right" size={10} color="white" />
+        {/* HEADER */}
+        <View style={categoryStyle.header}>
+          <Text
+            style={[
+              styles.title_white,
+              {
+                flexWrap: "wrap",
+                maxWidth: "100%",
+                width: undefined,
+                textAlign: "left",
+              },
+            ]}
+          >
+            Fondamentaux de la cybersécurité
           </Text>
-        </Gradient>
+        </View>
+        {/* BODY */}
+        <View style={categoryStyle.body}>
+          <View style={categoryStyle.bodyLeftDiv}>
+            <Text style={{ color: "white", marginBottom: 10 }}>
+              Apprenez les bases de la cybersécurité et protégez vos données
+            </Text>
+            <View style={{ width: "80%" }}>
+              <Gradient onPress={() => console.log("yo")}>
+                <Text style={styles.title_white}>
+                  Continuer{" "}
+                  <FontAwesome name="arrow-right" size={10} color="white" />
+                </Text>
+              </Gradient>
+            </View>
+          </View>
+          <View style={categoryStyle.bodyRightDiv}>
+            <Image
+              source={require("../assets/images/mock1.jpg")}
+              style={{ width: "100%", height: "100%", borderRadius: 10 }}
+            />
+          </View>
+        </View>
+
+        {/* FOOTER */}
       </LinearGradient>
     </View>
   );
@@ -48,6 +82,21 @@ const categoryStyle = StyleSheet.create({
   },
   header: {
     width: "100%",
-    height: "20%",
+    height: "25%",
+  },
+  body: {
+    width: "100%",
+    height: "65%",
+    flexDirection: "row",
+  },
+  bodyLeftDiv: {
+    width: "60%",
+    height: "100%",
+    flexDirection: "column",
+  },
+  bodyRightDiv: {
+    width: "40%",
+    height: "100%",
+    borderRadius: 10,
   },
 });
